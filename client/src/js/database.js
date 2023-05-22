2b786415f0bf41fa8611ac763f8ac3a7f3af20db
alboy77 <al.boucetta@yahoo.com>
2023-05-21 21:02:59 -0400

    get logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,13 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 
 export const getDb = async () => {
-  
-}
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jateSo', 'readonly');
+  const store = tx.objectStore('jateSo');
+  const request = store.get(1);
+  const result = await request;
+  return result?.content;
+};
 
 initdb();
